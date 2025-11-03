72cce78a773b79aae3ea52f2f515481ba122b0ea
Vitor Guidi <vitorguidi@gmail.com>
2025-11-03 02:33:54 -0300

    Adding simple yield

--- cox-coro/main.go
+++ cox-coro/main.go
@@ -1,6 +1,6 @@
 package main
 
-func New[In, Out any](calculation func(In) Out) (resumer func(In) Out) {
+func New[In, Out any](calculation func(In, func(Out) In) Out) (resumer func(In) Out) {
 	cin := make(chan In)
 	cout := make(chan Out)
 
@@ -9,18 +9,31 @@ func New[In, Out any](calculation func(In) Out) (resumer func(In) Out) {
 		return <-cout
 	}
 
+	yield := func(out Out) (in In) {
+		cout <- out
+		return <-cin
+	}
+
 	go func() {
-		cout <- calculation(<-cin)
+		cout <- calculation(<-cin, yield)
 	}()
 
 	return resume
 }
 
-func double(val int) (double_of_val int) {
-	return 2 * val
+// We can understand the first parameter as the initial value
+func mult_by_two(val int, yield func(int) int) int {
+	for val >= 0 {
+		val = yield(2 * val)
+	}
+	println("leaving coro")
+	return val
 }
 
 func main() {
-	doubling_coro := New(double)
-	println(doubling_coro(2))
+	resume := New(mult_by_two)
+	for i := 0; i < 3; i++ {
+		println(resume(i))
+	}
+	resume(-1)
 }
